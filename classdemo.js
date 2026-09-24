31d517ef10c349b15b8b94dc50a9d8c1d6c97d92
Rajnish Mishra <rm924581@gmail.com>
2026-09-24 21:56:10 +0530

    Class

---
+++ classdemo.js
@@ -0,0 +1,27 @@
+
+class Product {
+
+     name;
+     price;
+     category;
+     description;
+     rating;
+
+
+     addToCart() {
+          console.log("Product added to cart");
+     }
+
+     removeFromCart() {
+          console.log("Product removed from cart");
+     }
+
+     displayProduct() {
+          console.log("Product displayed");
+     }
+
+     buyProduct() {
+          console.log("Product bought");
+     } 
+
+}
