a71a535f16913c8f9dfa1ef5b361c94cd01c36a5
Rajnish Mishra <rm924581@gmail.com>
2026-09-26 19:31:29 +0530

    classAndObject

--- classAndObject.js
+++ classAndObject.js
@@ -1,18 +1,22 @@
 
 class Product {
 
-     // name;
-     // price;
-     // category;
-     // description;
-     // rating;
+     #name;
+     #price;
+     category;
+     description;
+     rating; 
 
      constructor(ProductName,ProductPrice, ProductCategory, ProductDescription, ProductRating) {
-          this.name = ProductName;
-          this.price = ProductPrice;
+          this.#name = ProductName;
+          this.#price = ProductPrice;
           this.category = ProductCategory;
           this.description = ProductDescription;
           this.rating = ProductRating;
+     }
+
+     displayProduct() {
+          console.log("Product displayed", this.#name, this.#price, this.description); 
      }
 
      addToCart() {
@@ -23,15 +27,12 @@ class Product {
           console.log("Product removed from cart");
      }
 
-     displayProduct() {
-          console.log("Product displayed");
-     }
-
      buyProduct() {
           console.log("Product bought");
      } 
 
 }
 
-let iphone = new Product("iphone 12", 125000, "Mobile", "Apple iphone 12", 4.5); 
-console.log(iphone)
+let iphone = new Product("iphone 12", 125000, "Mobile", "Apple", 4.5); 
+console.log(iphone);
+iphone.displayProduct();
